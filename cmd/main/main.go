7669ed899c15d014482da9c4b25a783a81ce4dd8
Shubham Gupta <gshubham030@gmail.com>
2022-08-09 17:41:28 +0530

    unformatted code

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -1,16 +1,17 @@
 package main
 
-import(
+import (
 	"log"
 	"net/http"
+
+	"github.com/akhil/go-bookstore/pkg/routes"
 	"github.com/gorilla/mux"
 	_ "github.com/jinzhu/gorm/dialects/mysql"
-	"github.com/akhil/go-bookstore/pkg/routes"
 )
 
-func main(){
+func main() {
 	r := mux.NewRouter()
 	routes.RegisterBookStoreRoutes(r)
-	http.Handle("/", r)
+		http.Handle("/", r)
 	log.Fatal(http.ListenAndServe("localhost:9010", r))
 }
